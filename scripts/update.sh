9c0abbf8924ade6f56087fbe16e9daace26f7663
Greg Tam <gtam@arrow.com>
2015-11-02 17:28:35 -0800

    fixed the update script due to changes in repo name

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,8 +4,8 @@
 
 BASE_DRAGONBOARD_DIR="/home/linaro/Documents"
 AWS_IOT_DEVICE_DIR="aws-iot-device-sdk"
-DRAGONPULSE_DIR="aws-iot-thing-sdk-dragonpulse-js"
-DRAGONCONNECT_DIR="aws-iot-thing-sdk-dragonconnect-c"
+DRAGONPULSE_DIR="aws-iot-dragonpulse-js"
+DRAGONCONNECT_DIR="aws-iot-dragonconnect-c"
 AWS_IOT_JS_DIR="aws-iot-device-sdk-js"
 AWS_IOT_C_DIR="aws-iot-device-sdk-embedded-C"
 
@@ -20,26 +20,26 @@ cd $BASE_DRAGONBOARD_DIR
 if [ -d "$DRAGONPULSE_DIR" ]; then
 	git pull
 else
-	git clone git://github.com/ArrowElectronics/aws-iot-thing-sdk-dragonpulse-js.git
+	git clone git://github.com/ArrowElectronics/$DRAGONPULSE_DIR.git
 fi
 
 # install/update dragonconnect demo
 if [ -d "$DRAGONCONNECT_DIR" ]; then
 	git pull
 else
-	git clone git://github.com/ArrowElectronics/aws-iot-thing-sdk-dragonconnect-c.git
+	git clone git://github.com/ArrowElectronics/$DRAGONCONNECT_DIR.git
 fi
 
 # install/update aws iot sdk javascript
 if [ -d "$AWS_IOT_JS_DIR" ]; then
 	git pull
 else
-	git clone git://github.com/aws/aws-iot-device-sdk-js.git
+	git clone git://github.com/aws/$AWS_IOT_JS_DIR.git
 fi
 
 # install/update aws iot sdk embedded c
 if [ -d "$AWS_IOT_C_DIR" ]; then
 	git pull
 else
-	git clone git://github.com/aws/aws-iot-device-sdk-embedded-C.git
+	git clone git://github.com/aws/$AWS_IOT_C_DIR.git
 fi
