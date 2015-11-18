a8e24bd9f27696fc43bb23066eefb425b98b95b3
Greg Tam <gtam@arrow.com>
2015-11-18 22:58:31 +0800

    updated with variable path and versioning

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,81 +1,104 @@
 #!/bin/bash
 
-# we assume we are sudo already
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+# we assume that nodejs, build-essentials is installed already...
+# in this script we will install and update all the repositories to a certain version
 
 BASE_DRAGONBOARD_DIR="/home/linaro/Documents"
-IS_FORCE="n"
-IS_INSTALL="n"
+AWS_IOT_DEVICE_DIR="aws-iot-device-sdk"
+DRAGONPULSE_DIR="aws-iot-dragonpulse-js"
+DRAGONCONNECT_DIR="aws-iot-dragonconnect-c"
+AWS_IOT_JS_DIR="aws-iot-device-sdk-js"
+AWS_IOT_C_DIR="aws-iot-device-sdk-embedded-C"
+DEFAULT_VERSION="v1.0.0"
+DEFAULT_AWS_VERSION="v1.0.7"
 
-if [ "$1" == "-f" ];then
-    IS_FORCE="y"
-else 
-    IS_FORCE="n"
+echo -e "Install system essentials using apt-get? (y=yes, n=no):"
+read iInstall
+
+if [ "$iInstall" == "y" ] ; then
+    if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+   fi
+   
+   apt-get install python2.7 nano iftop 
 fi
 
-cd $BASE_DRAGONBOARD_DIR
+#ask for path to install to
+echo -e "Provide a directory to install in (/home/linaro/Documents is the default):"
+read pPath
 
-if [ "$IS_FORCE" == "y" ];then
-    IS_INSTALL="y"
-    echo "forcing NodeJS install"
+if [ "$pPath" == "" ] ; then
+  echo "Using default path '/home/linaro/Documents'"
 else
-    CURRENT_NODE_VERSION=`node --version | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
-    #do a version check
-    if [ "$CURRENT_NODE_VERSION" == "" ] ; then
-        IS_INSTALL="y"
-    else
-        echo "NodeJS v$CURRENT_NODE_VERSION exists"
-    fi
+  echo "Using custom path $pPath"
+  BASE_DRAGONBOARD_DIR=$pPath
 fi
 
-if [ "$IS_INSTALL" == "y" ] ; then
-   if [ ! -d "tmp" ]; then
-    mkdir -p tmp
-   fi
-   
-   #relative to base
-   cd tmp
-   # currently node is not in the sources for apt-get, or this would be easier
-   # we'll pull directly from the node site and install into /home/linaro/share
-   # we need the arm64 version
-   # https://nodejs.org/dist/latest/node-v4.2.1-linux-arm64.tar.gz
-   NODE_LIB_ARCHIVE_FILE=`wget -q https://nodejs.org/dist/latest/ -O - | grep arm64.tar.gz | awk -F\" '{print $2}'`
-   NODE_VERSION=`echo $NODE_LIB_ARCHIVE_FILE | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
+if [ -d "$BASE_DRAGONBOARD_DIR" ]; then
 
-   echo "installing NodeJS $NODE_VERSION..."
-  
-   wget https://nodejs.org/dist/latest/$NODE_LIB_ARCHIVE_FILE
-   tar -xf $NODE_LIB_ARCHIVE_FILE
-   
-   if [ ! -d /home/linaro/share ];then
-    mkdir -p /home/linaro/share
-   fi
- 
-   mv node-v$NODE_VERSION-linux-arm64 /home/linaro/share
-   
-   #check if there are current symlinks
-   if [ -a /usr/local/bin/node ];then
-    rm /usr/local/bin/node
-   fi
-   
-   if [ -a /usr/local/bin/npm ];then
-    rm /usr/local/bin/npm
-   fi
-   
-   #create the symlinks
-   ln -s /home/linaro/share/node-v$NODE_VERSION-linux-arm64/bin/node /usr/local/bin/node
-   ln -s /home/linaro/share/node-v$NODE_VERSION-linux-arm64/lib/node_modules/npm/bin/npm-cli.js /usr/local/bin/npm
-   
-   #fix the permissions
-   chown -R linaro:linaro /home/linaro/share
-   chmod 777 /usr/local/bin/node
-   chmod 777 /usr/local/bin/npm
-   
-   #cleanup
-   cd $BASE_DRAGONBOARD_DIR
-   rm -Rf tmp
+  #ask for arrow dragonboard version to install
+  echo -e "DragonBoard Version to install ($DEFAULT_VERSION is the default):"
+  read pVersion
+
+  if [ "$pVersion" != "" ] ; then
+    DEFAULT_VERSION=$pVersion
+  fi
+
+  echo "Installing Arrow DragonBoard version $DEFAULT_VERSION"
+
+  #ask for aws version to install
+  echo -e "Amazon AWS Version to install ($DEFAULT_AWS_VERSION is the default):"
+  read pVersion
+
+  if [ "$aVersion" != "" ] ; then
+    DEFAULT_AWS_VERSION=$aVersion
+  fi
+
+  echo "Installing Amazon AWS version $DEFAULT_AWS_VERSION"
+
+  # update self/install self
+  cd $SCRIPTPATH
+  cd ..
+  git pull
+  git checkout tags/$DEFAULT_VERSION
+
+  # navigate to the base directory
+  cd $BASE_DRAGONBOARD_DIR
+
+  # install/update dragonpulse demo
+  if [ -d "$DRAGONPULSE_DIR" ]; then
+    git pull
+    git checkout tags/$DEFAULT_VERSION
+  else
+    git clone git://github.com/ArrowElectronics/$DRAGONPULSE_DIR.git
+  fi
+
+  # install/update dragonconnect demo
+  if [ -d "$DRAGONCONNECT_DIR" ]; then
+    git pull
+    git checkout tags/$DEFAULT_VERSION
+  else
+    git clone git://github.com/ArrowElectronics/$DRAGONCONNECT_DIR.git
+  fi
+
+  # install/update aws iot sdk javascript
+  if [ -d "$AWS_IOT_JS_DIR" ]; then
+    git pull
+    git checkout tags/$DEFAULT_AWS_VERSION
+  else
+    git clone git://github.com/aws/$AWS_IOT_JS_DIR.git
+  fi
+
+  # install/update aws iot sdk embedded c
+  if [ -d "$AWS_IOT_C_DIR" ]; then
+    git pull
+    git checkout tags/$DEFAULT_AWS_VERSION
+  else
+    git clone git://github.com/aws/$AWS_IOT_C_DIR.git
+  fi
+else
+  echo "Please make sure the directory '$BASE_DRAGONBOARD_DIR' is accesible"
 fi
-
-# install other things
-apt-get update
-apt-get upgrade
-apt-get install gcc g++ python2.7 build-essential openssl libssl-dev nano iftop 
