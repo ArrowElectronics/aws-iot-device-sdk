8fe2b218438c6591aeb7076a624d0e5cdcd1bb8f
Greg Tam <gtam@arrow.com>
2016-03-11 00:07:41 -0800

    added defaultjre and removing oxygen

--- scripts/beaglebone.sh
+++ scripts/beaglebone.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 
+TISENSE="aws-iot-tisense-js"
+DEFAULT_SENSE_VERSION="1.0.0"
+
+#ask for arrow dragonboard version to install
+echo -e "TiSense Version to install ($DEFAULT_SENSE_VERSION is the default):"
+read pVersion
+
+if [ "$pVersion" != "" ] ; then
+DEFAULT_SENSE_VERSION=$pVersion
+fi
+
+echo "***Configuring system with additional dependencies..."
+
+#we need a higher version of nodejs
+sudo apt-get remove node nodejs npm
+
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install locales resolvconf bluetooth bluez curl unzip python virtualenv
+sudo apt-get install locales resolvconf bluetooth bluez curl unzip python virtualenv default-jre
 
+#remove some stuff to free up room
+sudo apt-get remove oxygen-icon-theme
+ 
 #clean up
 sudo apt-get autoremove
 
 cd $BASE_DEVICE_DIR
+
+
+#check to see if online, if not online we cant do anything
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+    #online, continue
+else
+    echo -e "No internet connection, please follow the reconnect instructions before continuning..."
+    exit 1
+fi
+
 
 #see if node exists.
 #TODO (:gtam)
@@ -23,7 +53,7 @@ cd $BASE_DEVICE_DIR
 NODE_LIB_ARCHIVE_FILE=`wget -q https://nodejs.org/dist/latest-v4.x/ -O - | grep $DEVICE_ARCH.tar.xz | awk -F\" '{print $2}'`
 NODE_VERSION=`echo $NODE_LIB_ARCHIVE_FILE | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/'`
 
-echo "installing NodeJS $NODE_VERSION..."
+echo "***Installing NodeJS $NODE_VERSION..."
  
 if [ ! -d tmp ] ; then	
     mkdir tmp
@@ -40,12 +70,12 @@ fi
 
 mv node-v$NODE_VERSION-linux-$DEVICE_ARCH $BASE_DEVICE_DIR/share
 
-if [ -a /usr/local/bin/node ];then		
-    sudo rm /usr/local/bin/node		
+if [ -f /usr/local/bin/node ]; then		
+    sudo rm -Rf /usr/local/bin/node		
 fi		
  		
-if [ -a /usr/local/bin/npm ];then		
-    sudo rm /usr/local/bin/npm		
+if [ -f /usr/local/bin/npm ]; then		
+    sudo rm -Rf /usr/local/bin/npm		
 fi		
 		
 #create the symlinks		
@@ -58,3 +88,26 @@ sudo chmod 777 /usr/local/bin/npm
 
 cd $BASE_DEVICE_DIR		
 rm -Rf tmp
+
+npm install -g grunt-cli
+
+echo "***Installing Arrow TiSense Example version $DEFAULT_SENSE_VERSION (tisense)"
+
+# navigate to the base directory
+cd $BASE_DEVICE_DIR
+
+#-------------------
+# install/update tisense demo
+#-------------------
+if [ ! -d "$ARROW_DIR/$TISENSE" ]; then
+    mkdir -p $ARROW_DIR
+    cd $ARROW_DIR
+    git clone https://github.com/ArrowElectronics/$TISENSE.git $TISENSE
+fi
+
+cd $BASE_DEVICE_DIR/$ARROW_DIR/$TISENSE
+git pull
+git checkout tags/$DEFAULT_SENSE_VERSION
+
+#reset the path
+cd $BASE_DEVICE_DIR
